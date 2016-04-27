1cdb60ea6a0faf439cc809f00fd0a3e17710fb5b
shift <ivanlampert@hotmail.com>
2016-04-26 23:51:08 -0300

    Celeeeeeeeeeeeeeeeeeeeeeeragit add .!

--- HackathonV2/src/com/stefanini/hackathon2/servicos/LivroServico.java
+++ HackathonV2/src/com/stefanini/hackathon2/servicos/LivroServico.java
@@ -23,49 +23,13 @@ public class LivroServico {
 	}
 
 	@Transacional
+	public List<Livro> carregaTodosLivrosDoBanco() {
+		return repositorio.todosLivros();
+	}
+	
+	@Transacional
 	public Livro pesquisarPorID(Integer id) {
 		return repositorio.pesquisarPorID(id);
-	}
-	
-	@Transacional
-	public List<Livro> carregaTodosLivrosDoBanco() {
-		return repositorio.todosLivros();
-	}
-
-	@Transacional
-	public void deletar(Livro livro) {
-		repositorio.remover(livro);;
-	}
-	
-}
-=======
-package com.stefanini.hackathon2.servicos;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
-import com.stefanini.hackathon2.entidades.Livro;
-import com.stefanini.hackathon2.repositorios.LivroRepositorio;
-import com.stefanini.hackathon2.transacao.Transacional;
-
-public class LivroServico {
-
-	@Inject
-	private LivroRepositorio repositorio;
-	
-	@Transacional
-	public void salvar(Livro livro) {
-		if (livro.getId() == null) {
-			repositorio.inserir(livro);
-		} else {
-			repositorio.atualizar(livro);
-		}
-	}
-
-	@Transacional
-	public List<Livro> carregaTodosLivrosDoBanco() {
-		return repositorio.todosLivros();
 	}
 
 	@Transacional
